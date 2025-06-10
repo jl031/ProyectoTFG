e0a591cd00d1e8568441240551714e00d708d6b1
Jinghao <jlin031@g.educaand.es>
2025-06-10 16:42:32 +0200

    stats

--- Assets/HealthBarScript.cs
+++ Assets/HealthBarScript.cs
@@ -6,20 +6,25 @@ public class HealthBarScript : MonoBehaviour
 
     public Image hpBar;
     public Text hpText;
+    public Text scoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float maxHP = Player.instance.playerStats.maxHealth;
-        float HP = Player.instance.playerStats.health;
+        PlayerStats stats = Player.instance.playerStats;
+        float maxHP = stats.maxHealth;
+        float HP = stats.health;
+
+        RunData data  = RunDataHandler.runData;
 
         hpBar.fillAmount = HP/maxHP;
-                
+
+        scoreText.text = $"Score: {data.score}\nMoney: {data.money}";                
     }
 }
